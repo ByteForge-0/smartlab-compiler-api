86643a4a39b944f3f26b230a4919857b41424f3b
DARKER <foodiestudip@gmail.com>
2026-06-30 08:01:34 +0530

    compiler api initial

---
+++ main.py
@@ -0,0 +1,106 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import subprocess
+import tempfile
+import shutil
+import os
+
+
+app=FastAPI()
+
+
+class Request(BaseModel):
+
+    code:str
+
+
+@app.post("/run")
+
+def run(data:Request):
+
+    folder=tempfile.mkdtemp()
+
+    try:
+
+        cpp=f"{folder}/main.cpp"
+
+        with open(cpp,"w") as f:
+
+            f.write(data.code)
+
+
+        compile_result=subprocess.run(
+
+            [
+
+            "g++",
+
+            cpp,
+
+            "-o",
+
+            f"{folder}/app"
+
+            ],
+
+            capture_output=True,
+
+            text=True
+
+        )
+
+
+        if compile_result.returncode!=0:
+
+            return {
+
+                "output":
+
+                compile_result.stderr
+
+            }
+
+
+        result=subprocess.run(
+
+            [
+
+            f"{folder}/app"
+
+            ],
+
+            capture_output=True,
+
+            text=True,
+
+            timeout=5
+
+        )
+
+
+        return {
+
+            "output":
+
+            result.stdout
+
+        }
+
+
+    except subprocess.TimeoutExpired:
+
+        return {
+
+            "output":
+
+            "Execution Timeout"
+
+        }
+
+
+    finally:
+
+        shutil.rmtree(
+            folder,
+            ignore_errors=True
+        )
